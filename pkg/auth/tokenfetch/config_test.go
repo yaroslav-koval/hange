527ad56177d173c5931c363b85567b475383a84e
nocle <koval.yar.val@gmail.com>
2025-12-07 14:36:01 +0100

    added tests for `auth` adapters

---
+++ pkg/auth/tokenfetch/config_test.go
@@ -0,0 +1,51 @@
+package tokenfetch
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	configurator_mock "github.com/yaroslav-koval/hange/mocks/configurator"
+	"github.com/yaroslav-koval/hange/pkg/config/consts"
+)
+
+func TestFetch(t *testing.T) {
+	t.Parallel()
+
+	cfg := configurator_mock.NewMockConfigurator(t)
+
+	fetcher := NewConfigTokenFetcher(cfg)
+
+	cfg.EXPECT().ReadField(consts.AuthTokenPath).Return("token-value")
+
+	token, err := fetcher.Fetch()
+	require.NoError(t, err)
+	require.Equal(t, "token-value", token)
+}
+
+func TestNilToken(t *testing.T) {
+	t.Parallel()
+
+	cfg := configurator_mock.NewMockConfigurator(t)
+
+	fetcher := NewConfigTokenFetcher(cfg)
+
+	cfg.EXPECT().ReadField(consts.AuthTokenPath).Return(nil)
+
+	token, err := fetcher.Fetch()
+	require.ErrorIs(t, err, ErrTokenNotSet)
+	require.Empty(t, token)
+}
+
+func TestInvalidFormat(t *testing.T) {
+	t.Parallel()
+
+	cfg := configurator_mock.NewMockConfigurator(t)
+
+	fetcher := NewConfigTokenFetcher(cfg)
+
+	cfg.EXPECT().ReadField(consts.AuthTokenPath).Return(1234)
+
+	token, err := fetcher.Fetch()
+	require.ErrorIs(t, err, errInvalidFormat)
+	require.Empty(t, token)
+}
--- pkg/auth/tokenstore/config_test.go
+++ pkg/auth/tokenstore/config_test.go
@@ -4,50 +4,34 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
+	configurator_mock "github.com/yaroslav-koval/hange/mocks/configurator"
 	"github.com/yaroslav-koval/hange/pkg/config/consts"
 )
 
 func TestStore(t *testing.T) {
 	t.Parallel()
 
-	mockCfg := &fakeConfigurator{}
+	cfg := configurator_mock.NewMockConfigurator(t)
 
-	storer := NewConfigTokenStorer(mockCfg)
-	err := storer.Store("token")
+	storer := NewConfigTokenStorer(cfg)
 
+	cfg.EXPECT().WriteField(consts.AuthTokenPath, "token-value").Return(nil)
+
+	err := storer.Store("token-value")
 	require.NoError(t, err)
-	assert.Equal(t, consts.AuthTokenPath, mockCfg.wroteField)
-	assert.Equal(t, "token", mockCfg.wroteValue)
 }
 
-func TestStoreReturnsError(t *testing.T) {
+func TestFail(t *testing.T) {
 	t.Parallel()
 
-	mockCfg := &fakeConfigurator{writeErr: errors.New("write error")}
+	cfg := configurator_mock.NewMockConfigurator(t)
 
-	storer := NewConfigTokenStorer(mockCfg)
-	err := storer.Store("token")
+	storer := NewConfigTokenStorer(cfg)
 
-	require.EqualError(t, err, "write error")
-	assert.Equal(t, consts.AuthTokenPath, mockCfg.wroteField)
-	assert.Equal(t, "token", mockCfg.wroteValue)
-}
+	expErr := errors.New("not valid token")
+	cfg.EXPECT().WriteField(consts.AuthTokenPath, "token-value").Return(expErr)
 
-type fakeConfigurator struct {
-	wroteField string
-	wroteValue any
-	writeErr   error
-}
-
-func (f *fakeConfigurator) WriteField(field string, value any) error {
-	f.wroteField = field
-	f.wroteValue = value
-	return f.writeErr
-}
-
-func (f *fakeConfigurator) ReadField(field string) any {
-	return nil
+	err := storer.Store("token-value")
+	require.ErrorIs(t, err, expErr)
 }
