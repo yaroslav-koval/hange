95c26564ea9097a4c204ac11adc1817c53305581
nocle <koval.yar.val@gmail.com>
2025-12-13 23:08:04 +0100

    producer optimization to decrease load

--- pkg/fileprovider/directory/filer.go
+++ pkg/fileprovider/directory/filer.go
@@ -104,6 +104,15 @@ func (d *directoryReader) readAndSendFile(
 	for range workersCount {
 		eg.Go(func() error {
 			for {
+				// This function has 2 selects.
+				// First (right below) is needed to instantly stop the logic to avoid heavy operation of file reads.
+				// Second (at the end) is used to create non-blocking behaviour.
+				select {
+				case <-ctx.Done():
+					return context.Canceled
+				default:
+				}
+
 				i := fnIndex.Add(1)
 				if i >= int32(len(filePaths)) {
 					return nil
